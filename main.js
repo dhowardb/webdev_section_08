e2c4c14c93a18b1a16a5f61d148f1b2d18411da1
dhowardb <howard.others6@gmail.com>
2022-10-30 21:59:43 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+let age = 32;
+let userName = "Max";
+let hobbies = ["Sports", "Cooking", "Reading"];
+
+let job = {
+  title: "Developer",
+  place: "New York",
+  salary: "100000",
+};
+
+let jobs = [
+  {
+    title: "Developer",
+    place: "New York",
+    salary: "100000",
+  },
+  {
+    title: "Tester",
+    place: "New York",
+    salary: "50000",
+  },
+];
+
+age = 45;
+
+let adultYears = age - 18;
+console.log(adultYears);
+
+console.log(job.title);
+
+console.log(jobs[0].salary);
