baf21ec3665ca0aa5799da758fdf2cd2dae95c2c
dhowardb <howard.others6@gmail.com>
2022-11-04 12:00:25 +0800

    3rd party packages for css and js

---
+++ scripts/parallax.js
@@ -0,0 +1,11 @@
+var image = document.getElementsByClassName("thumbnail");
+new simpleParallax(image, {
+  scale: 1.6,
+  delay: 0.1,
+});
+
+// var images = document.querySelectorAll('img');
+// new simpleParallax(images);
+
+// const imageElement = document.getElementById("main-image");
+// new simpleParallax(imageElement);
